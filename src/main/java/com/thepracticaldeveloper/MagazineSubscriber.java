d54499099f7ddb1b119dc57f41e9ec75d30bbf53
Moises Macero <mechero@users.noreply.github.com>
2018-02-28 20:38:22 +0100

    Changed formatting to look better in printed versions

--- src/main/java/com/thepracticaldeveloper/MagazineSubscriber.java
+++ src/main/java/com/thepracticaldeveloper/MagazineSubscriber.java
@@ -8,71 +8,75 @@ import org.slf4j.LoggerFactory;
 
 public class MagazineSubscriber implements Flow.Subscriber<Integer> {
 
-    public static final String JACK = "Jack";
-    public static final String PETE = "Pete";
+  public static final String JACK = "Jack";
+  public static final String PETE = "Pete";
 
-    private static final Logger log = LoggerFactory.getLogger(MagazineSubscriber.class);
+  private static final Logger log = LoggerFactory.
+    getLogger(MagazineSubscriber.class);
 
-    private final long sleepTime;
-    private final String subscriberName;
-    private Flow.Subscription subscription;
-    private int nextMagazineExpected;
-    private int totalRead;
+  private final long sleepTime;
+  private final String subscriberName;
+  private Flow.Subscription subscription;
+  private int nextMagazineExpected;
+  private int totalRead;
 
-    MagazineSubscriber(final long sleepTime, final String subscriberName) {
-        this.sleepTime = sleepTime;
-        this.subscriberName = subscriberName;
-        this.nextMagazineExpected = 1;
-        this.totalRead = 0;
+  MagazineSubscriber(final long sleepTime, final String subscriberName) {
+    this.sleepTime = sleepTime;
+    this.subscriberName = subscriberName;
+    this.nextMagazineExpected = 1;
+    this.totalRead = 0;
+  }
+
+  @Override
+  public void onSubscribe(final Flow.Subscription subscription) {
+    this.subscription = subscription;
+    subscription.request(1);
+  }
+
+  @Override
+  public void onNext(final Integer magazineNumber) {
+    if (magazineNumber != nextMagazineExpected) {
+      IntStream.range(nextMagazineExpected, magazineNumber).forEach(
+        (msgNumber) ->
+          log("Oh no! I missed the magazine " + msgNumber)
+      );
+      // Catch up with the number to keep tracking missing ones
+      nextMagazineExpected = magazineNumber;
     }
+    log("Great! I got a new magazine: " + magazineNumber);
+    takeSomeRest();
+    nextMagazineExpected++;
+    totalRead++;
 
-    @Override
-    public void onSubscribe(final Flow.Subscription subscription) {
-        this.subscription = subscription;
-        subscription.request(1);
+    log("I'll get another magazine now, next one should be: " +
+      nextMagazineExpected);
+    subscription.request(1);
+  }
+
+  @Override
+  public void onError(final Throwable throwable) {
+    log("Oops I got an error from the Publisher: " + throwable.getMessage());
+  }
+
+  @Override
+  public void onComplete() {
+    log("Finally! I completed the subscription, I got in total " +
+      totalRead + " magazines.");
+  }
+
+  private void log(final String logMessage) {
+    log.info("<=========== [" + subscriberName + "] : " + logMessage);
+  }
+
+  public String getSubscriberName() {
+    return subscriberName;
+  }
+
+  private void takeSomeRest() {
+    try {
+      Thread.sleep(sleepTime);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
     }
-
-    @Override
-    public void onNext(final Integer magazineNumber) {
-        if (magazineNumber != nextMagazineExpected) {
-            IntStream.range(nextMagazineExpected, magazineNumber).forEach((msgNumber) ->
-                log("Oh no! I missed the magazine " + msgNumber)
-            );
-            // Catch up with the number to keep tracking missing ones
-            nextMagazineExpected = magazineNumber;
-        }
-        log("Great! I got a new magazine: " + magazineNumber);
-        takeSomeRest();
-        nextMagazineExpected++;
-        totalRead++;
-
-        log("I'll get another magazine now, next one should be: " + nextMagazineExpected);
-        subscription.request(1);
-    }
-
-    @Override
-    public void onError(final Throwable throwable) {
-        log("Oops I got an error from the Publisher: " + throwable.getMessage());
-    }
-
-    @Override
-    public void onComplete() {
-        log("Finally! I completed the subscription, I got in total " + totalRead + " magazines.");
-    }
-
-    private void log(final String logMessage) {
-        log.info("<=========== [" + subscriberName + "] : " + logMessage);
-    }
-
-    public String getSubscriberName() {
-        return subscriberName;
-    }
-
-    private void takeSomeRest() {
-        try {
-            Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  }
 }
--- src/main/java/com/thepracticaldeveloper/logutils/ColorConsoleAppender.java
+++ src/main/java/com/thepracticaldeveloper/logutils/ColorConsoleAppender.java
@@ -7,18 +7,18 @@ import org.apache.log4j.spi.LoggingEvent;
 
 public class ColorConsoleAppender extends ConsoleAppender {
 
-    @Override
-    protected void subAppend(final LoggingEvent event) {
-        int color = 36;
-        if (event.getRenderedMessage().contains(MagazineSubscriber.JACK)) {
-            color = 32;
-        } else if (event.getRenderedMessage().contains(MagazineSubscriber.PETE)) {
-            color = 31;
-        }
-        qw.write("\u001b[0;" + color + "m");
-        super.subAppend(event);
-        qw.write("\u001b[m");
-        if (this.immediateFlush) qw.flush();
+  @Override
+  protected void subAppend(final LoggingEvent event) {
+    int color = 36;
+    if (event.getRenderedMessage().contains(MagazineSubscriber.JACK)) {
+      color = 32;
+    } else if (event.getRenderedMessage().contains(MagazineSubscriber.PETE)) {
+      color = 31;
     }
+    qw.write("\u001b[0;" + color + "m");
+    super.subAppend(event);
+    qw.write("\u001b[m");
+    if (this.immediateFlush) qw.flush();
+  }
 
 }
